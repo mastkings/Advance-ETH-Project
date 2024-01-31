8f06c0f805f1cca6d3470496a8c1e1829532e837
mastkings <157817089+mastkings@users.noreply.github.com>
2024-01-31 06:57:28 +0100

    Update hardhat.config.js

--- Building-Your-First-DApp-Vesting/hardhat.config.js
+++ Building-Your-First-DApp-Vesting/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/9gO8RVbokTEMFpjvXe_UooQtrZbNJ79V",
-      accounts: ["9673488150c05380c2d245a4b7926252132489ecc9f19fd3513e926993cce2d1"],
+      accounts: ["Your Private key here"],
     },
   },
 };
